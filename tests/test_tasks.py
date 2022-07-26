a075ce42e81a303aa5bfc11c56c7b415d536c239
Kyle McCormick <kdmc@pm.me>
2022-07-26 11:44:13 -0400

    test: fix up cursory version of test_tasks

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,12 +1,13 @@
-# import re
+import re
 import unittest
 
-# from io import StringIO
-# from unittest.mock import patch
+from io import StringIO
+from unittest.mock import patch
 
-# from tests.helpers import TestContext, temporary_root
-# from tutor import config as tutor_config
-# from tutor.commands.tasks import run_task
+from tests.helpers import TestContext, temporary_root
+from tutor import config as tutor_config
+from tutor import hooks
+from tutor.commands.tasks import run_task
 
 
 class BuiltinTaskTests(unittest.TestCase):
@@ -14,39 +15,49 @@ class BuiltinTaskTests(unittest.TestCase):
     Test tasks that are defined within core Tutor
     (ie, not plugin-defined tasks).
 
-    TODO: Update these to work with new task output formats
+    TODO: Update these to tests to be more thorough. Currently they
+    are essentially just testing that task lookup and script printing are
+    working correctly.
     """
 
-    # @patch("sys.stdout", new_callable=StringIO)
-    # def test_initialise(self, mock_stdout: StringIO) -> None:
-    #     with temporary_root() as root:
-    #         context = TestContext(root)
-    #         config = tutor_config.load_full(root)
-    #         runner = context.job_runner(config)
-    #         jobs.initialise(runner)
-    #         output = mock_stdout.getvalue().strip()
-    #         self.assertTrue(output.startswith("Initialising all services..."))
-    #         self.assertTrue(output.endswith("All services initialised."))
+    @classmethod
+    def setUpClass(cls) -> None:
+        # TODO: We need to ensure that the CORE_READY signal is fired
+        # before running these tests, but is this really the best way to
+        # do it?
+        hooks.Actions.CORE_READY.do()
 
-    # @patch("sys.stdout", new_callable=StringIO)
-    # def test_import_demo_course(self, mock_stdout: StringIO) -> None:
-    #     with temporary_root() as root:
-    #         context = TestContext(root)
-    #         config = tutor_config.load_full(root)
-    #         runner = context.job_runner(config)
-    #         run_task(runner, "importdemocourse")
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_initialise(self, mock_stdout: StringIO) -> None:
+        with temporary_root() as root:
+            context = TestContext(root)
+            config = tutor_config.load_full(root)
+            runner = context.job_runner(config)
+            run_task(runner, "init")
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("./manage.py lms migrate", output)
+            self.assertIn("./manage.py cms migrate", output)
+            self.assertIn('echo "Initialising MySQL..."', output)
 
-    #         output = mock_stdout.getvalue()
-    #         service = re.search(r"Service: (\w*)", output)
-    #         commands = re.search(r"(-----)([\S\s]+)(-----)", output)
-    #         assert service is not None
-    #         assert commands is not None
-    #         self.assertEqual(service.group(1), "cms")
-    #         self.assertTrue(
-    #             commands.group(2)
-    #             .strip()
-    #             .startswith('sh -c \'echo "Loading settings $DJANGO_SETTINGS_MODULE"')
-    #         )
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_import_demo_course(self, mock_stdout: StringIO) -> None:
+        with temporary_root() as root:
+            context = TestContext(root)
+            config = tutor_config.load_full(root)
+            runner = context.job_runner(config)
+            run_task(runner, "importdemocourse")
+
+            output = mock_stdout.getvalue()
+            service = re.search(r"Service: (\w*)", output)
+            commands = re.search(r"(-----)([\S\s]+)(-----)", output)
+            assert service is not None
+            assert commands is not None
+            self.assertEqual(service.group(1), "cms")
+            self.assertTrue(
+                commands.group(2)
+                .strip()
+                .startswith('sh -c \'echo "Loading settings $DJANGO_SETTINGS_MODULE"')
+            )
 
     # def test_create_user_without_staff(self) -> None:
     #    command = jobs.create_user_command("superuser", False, "username", "email")
@@ -62,26 +73,50 @@ class BuiltinTaskTests(unittest.TestCase):
     #    )
     #    self.assertIn("set_password", command)
 
-    # @patch("sys.stdout", new_callable=StringIO)
-    # def test_set_theme(self, mock_stdout: StringIO) -> None:
-    #     with temporary_root() as root:
-    #         context = TestContext(root)
-    #         config = tutor_config.load_full(root)
-    #         runner = context.job_runner(config)
-    #         run_task(
-    #             runner,
-    #             "settheme",
-    #             args=["sample_theme", "-d", "domain1", "-d", "domain2"],
-    #         )
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_create_user(self, mock_stdout: StringIO) -> None:
+        with temporary_root() as root:
+            context = TestContext(root)
+            config = tutor_config.load_full(root)
+            runner = context.job_runner(config)
+            run_task(
+                runner,
+                "createuser",
+                args=["user1", "user1@example.com", "--staff", "--password", "abc"],
+            )
 
-    #         output = mock_stdout.getvalue()
-    #         service = re.search(r"Service: (\w*)", output)
-    #         commands = re.search(r"(-----)([\S\s]+)(-----)", output)
-    #         assert service is not None
-    #         assert commands is not None
-    #         self.assertEqual(service.group(1), "lms")
-    #         self.assertTrue(
-    #             commands.group(2)
-    #             .strip()
-    #             .startswith('sh -c \'theme=""\ndomains=""\nusage="')
-    #         )
+            output = mock_stdout.getvalue()
+            service = re.search(r"Service: (\w*)", output)
+            commands = re.search(r"(-----)([\S\s]+)(-----)", output)
+            assert service is not None
+            assert commands is not None
+            self.assertEqual(service.group(1), "lms")
+            self.assertTrue(
+                commands.group(2)
+                .strip()
+                .startswith('sh -c \'opts=""\nusername=""\nemail="')
+            )
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_set_theme(self, mock_stdout: StringIO) -> None:
+        with temporary_root() as root:
+            context = TestContext(root)
+            config = tutor_config.load_full(root)
+            runner = context.job_runner(config)
+            run_task(
+                runner,
+                "settheme",
+                args=["sample_theme", "-d", "domain1", "-d", "domain2"],
+            )
+
+            output = mock_stdout.getvalue()
+            service = re.search(r"Service: (\w*)", output)
+            commands = re.search(r"(-----)([\S\s]+)(-----)", output)
+            assert service is not None
+            assert commands is not None
+            self.assertEqual(service.group(1), "lms")
+            self.assertTrue(
+                commands.group(2)
+                .strip()
+                .startswith('sh -c \'theme=""\ndomains=""\nusage="')
+            )
