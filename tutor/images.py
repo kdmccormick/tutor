1a3abf81a1e3598522b49504d975f7fa88688ced
Kyle D. McCormick <kyle@axim.org>
2023-08-18 11:33:41 -0400

    feat!: assume BuildKit is available
    BuildKit replaces and improves the legacy Docker builder, which was
    deprecated back in Feb 2023. Assuming BuildKit allows us to simplify the
    Dockerfile and makes future build performance improvements easier. The
    Docker versions which Tutor recommends (v20+) all come with BuildKit, so
    
    As follow-up work, we will need to remove `is_buildkit_enabled` from
    the official plugins templates.
    
    Relevant discussion:
    https://github.com/overhangio/tutor/pull/868#issuecomment-1657961059

--- tutor/images.py
+++ tutor/images.py
@@ -4,8 +4,7 @@ from tutor import fmt, hooks, utils
 def build(path: str, tag: str, *args: str) -> None:
     fmt.echo_info(f"Building image {tag}")
     build_command = ["build", f"--tag={tag}", *args, path]
-    if utils.is_buildkit_enabled():
-        build_command.insert(0, "buildx")
+    build_command.insert(0, "buildx")
     command = hooks.Filters.DOCKER_BUILD_COMMAND.apply(build_command)
     utils.docker(*command)
 
